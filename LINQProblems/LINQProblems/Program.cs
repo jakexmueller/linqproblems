8b9346bf48a6a4868d41128e097033b6cc332c5d
Jacob <jakexmueller@gmail.com>
2018-06-25 10:40:22 -0500

    problem 1

--- LINQProblems/LINQProblems/Program.cs
+++ LINQProblems/LINQProblems/Program.cs
@@ -8,8 +8,19 @@ namespace LINQProblems
 {
     class Program
     {
+        delegate bool StringToBoolFunction(string word);
         static void Main(string[] args)
         {
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            var comparison = "th";
+            //bool result = words[i].Contains(comparison);
+            var wordsth = words.Where(w => w.Contains(comparison)).ToList();
+            foreach (var word in wordsth)
+            {
+                Console.WriteLine(word);
+
+            }
+            Console.ReadLine();
         }
     }
 }
